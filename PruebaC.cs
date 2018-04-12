db1e9f2334a6566bd95e00d9eb60341be7f3df3e
Franco Zanek <francozanek@hotmail.com>
2018-04-12 11:15:11 -0300

    modificaciones

--- PruebaC.cs
+++ PruebaC.cs
@@ -24,7 +24,7 @@ namespace Prueba
 			// The InitializeComponent() call is required for Windows Forms designer support.
 			//
 			InitializeComponent();
-			
+			//MODIFIQUE ESTO	
 			//
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
